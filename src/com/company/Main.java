c4f8a3bbc7d361df0f4046c813156c4eb0ee0e5f
Batur123 <batur.ozkoc@gmail.com>
2021-02-16 20:37:14 +0300

    SQLite Added for Users

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,47 +8,61 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import static com.company.Miscellaneous.*;
 
-import org.junit.Test;
-
 public class Main
 {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException
     {
-        //region WithoutUnitTestFunction
+        //Establish SQL connection.
+        SQLConnection.connect("UsersDatabase.db");
 
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-            LocalDateTime now = LocalDateTime.now();
+        //Create User Table
+        SQLConnection.createNewTable("UsersDatabase.db");
 
-            List<String> Users = Arrays.asList("Root", "Batuhan");
-            User User1 = new User("31972875-4a74-4022-bb78-2c6028cb2d6a","12345","Root","20e095a6-02fa-48b7-8c41-24bfcf3c7fb0");
-            User User2 = new User("88212c94-62b2-4cea-9996-04120c1ea87c","5544","Batuhan","1a55f567-43d1-4d4f-aa5a-e02972342a26");
+        //Insert Users
+        SQLConnection.InsertDatabase("31972875-4a74-4022-bb78-2c6028cb2d6a","Root","12345","NULLForNow");
+        SQLConnection.InsertDatabase("88212c94-62b2-4cea-9996-04120c1ea87c","Batuhan","111","NULLForNow");
+        SQLConnection.InsertDatabase("fae15d70-65f1-40be-87e5-9ca1d61728fc","Test1","222","NULLForNow");
+        SQLConnection.InsertDatabase("8279b24d-222e-4eba-b80c-8cc6d9546757","User1","333","NULLForNow");
+        SQLConnection.InsertDatabase("e5466c97-68b6-4867-bfe5-403e01ca89b3","DBAdmin","444","NULLForNow");
+        SQLConnection.InsertDatabase("3252a19b-e5e0-4970-8658-2114cbc9fddc","Faraday","555","NULLForNow");
+        SQLConnection.InsertDatabase("ee7000e6-11ee-45e8-8b89-70ac9e8eb4b9","User2","666","NULLForNow");
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print(ANSI_PURPLE+"Users: ");
-            for (int i = 0; i < Users.size(); i++)
+        //List All Users for Login
+        SQLConnection.ListAllUsers();
+
+        //Login
+        System.out.print(ANSI_RESET+ANSI_RED+"[Server]: "+ANSI_BLUE+"You need to login. Enter your username: "+ANSI_RESET);
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Get Username Input
+        String UsernameInput = scanner.nextLine();
+
+        if(SQLConnection.UsernamesAll.indexOf(UsernameInput) != -1)
+        {
+            System.out.print(ANSI_RESET+ANSI_RED+"[Server]: "+ANSI_BLUE+"Enter your password: "+ANSI_RESET);
+
+            //Get Password Input
+            String PasswordInput = scanner.nextLine();
+
+            //Login Function
+            User user = SQLConnection.LoginDatabase(UsernameInput,PasswordInput);
+
+            //Check If Username/Password True
+            if(user != null)
             {
-                System.out.print(ANSI_PURPLE+Users.get(i));
-                if(i != Users.size()-1)
-                {
-                    System.out.print(",");
-                }
-            }
-            System.out.println(" ");
-            System.out.println(ANSI_WHITE+"Select user with typing its name: "+ANSI_RESET);
-            String WhichUser = scanner.nextLine();
+                System.out.println(ANSI_RESET+ANSI_RED+"[Server]: "+ANSI_BLUE+"Confirmation succesfull. "+ANSI_RESET);
 
-            if(!Users.contains(WhichUser))
-            {
-                System.out.println(ANSI_RED+"You selected wrong username.");
-            }
-            else
-            {
+                //Datetime - Now
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+                LocalDateTime now = LocalDateTime.now();
+
                 boolean check = true;
 
                 while(check)
                 {
-                    System.out.print(ANSI_RESET+ANSI_GREEN+"Enter your command to the command line:"+ANSI_RESET);
+                    System.out.print(ANSI_RESET+ANSI_GREEN+"Enter your command to the command line: "+ANSI_RESET);
                     String Command = scanner.nextLine();
 
                     //Store Args of the Command in List.
@@ -59,7 +73,6 @@ public class Main
                     {
                         CommandArgs.add(a);
                     }
-
 
                     Process process;
 
@@ -75,12 +88,12 @@ public class Main
                             socket = new Socket(host.getHostName(), 9876);
                             oos = new ObjectOutputStream(socket.getOutputStream());
                             System.out.println("Sending quit request to Socket Server");
-                            User1.SetRecentCommand(Command);
+                            user.SetRecentCommand(Command);
 
                             //Send Variables to the Server
                             List<String> Info=new LinkedList<String>();
-                            Info.add(User1.GetUserName());
-                            Info.add(User1.GetRecentCommand());
+                            Info.add(user.GetUserName());
+                            Info.add(user.GetRecentCommand());
                             oos.writeObject(Info);
 
                             ois = new ObjectInputStream(socket.getInputStream());
@@ -103,18 +116,12 @@ public class Main
                                 socket = new Socket(host.getHostName(), 9876);
                                 oos = new ObjectOutputStream(socket.getOutputStream());
                                 System.out.println("Sending request to Socket Server");
-                                User1.SetRecentCommand(Command); //User2
-
-
-                                /* Important Note
-                                    We dont use SQL in that project so you need to change User1 to User2 if you want to run through multiple users. If we use
-                                    Database that would be very easy to optimize and use.
-                                 */
+                                user.SetRecentCommand(Command); //User2
 
                                 //Send Variables to the Server
                                 List<String> Info=new LinkedList<String>();
-                                Info.add(User1.GetUserName()); //User2
-                                Info.add(User1.GetRecentCommand()); //User2
+                                Info.add(user.GetUserName()); //User2
+                                Info.add(user.GetRecentCommand()); //User2
                                 oos.writeObject(Info);
 
                                 //Server Response
@@ -136,22 +143,11 @@ public class Main
                             }
                             finally
                             {
-                                if(WhichUser.contains("Root"))
-                                {
-                                    User1.SetRecentCommand(Command);
-                                    User1.SetRecentCommandDate(dtf.format(now));
-                                    User1.SetLog(User1.GetUserName()+" user wrote ("+User1.GetRecentCommand()+") command on "+User1.GetRecentCommandDate());
-                                    User1.SetListofCommandArgs(CommandArgs);
-                                    User1.SetOperatingSystem(OS);
-                                }
-                                else if(WhichUser.contains("Batuhan"))
-                                {
-                                    User2.SetRecentCommand(Command);
-                                    User2.SetRecentCommandDate(dtf.format(now));
-                                    User2.SetLog(User2.GetUserName()+" user wrote ("+User2.GetRecentCommand()+") command on "+User2.GetRecentCommandDate());
-                                    User2.SetListofCommandArgs(CommandArgs);
-                                    User2.SetOperatingSystem(OS);
-                                }
+                                user.SetRecentCommand(Command);
+                                user.SetRecentCommandDate(dtf.format(now));
+                                user.SetLog(user.GetUserName()+" user wrote ("+user.GetRecentCommand()+") command on "+user.GetRecentCommandDate());
+                                user.SetListofCommandArgs(CommandArgs);
+                                user.SetOperatingSystem(OS);
                             }
                         }
                         else
@@ -174,12 +170,12 @@ public class Main
                             socket = new Socket(host.getHostName(), 9876);
                             oos = new ObjectOutputStream(socket.getOutputStream());
                             System.out.println("Sending quit request to Socket Server");
-                            User1.SetRecentCommand(Command);
+                            user.SetRecentCommand(Command);
 
                             //Send Variables to the Server
                             List<String> Info=new LinkedList<String>();
-                            Info.add(User1.GetUserName());
-                            Info.add(User1.GetRecentCommand());
+                            Info.add(user.GetUserName());
+                            Info.add(user.GetRecentCommand());
                             oos.writeObject(Info);
                             ois = new ObjectInputStream(socket.getInputStream());
                             String ServerAnswer = (String) ois.readObject();
@@ -201,12 +197,12 @@ public class Main
                                 socket = new Socket(host.getHostName(), 9876);
                                 oos = new ObjectOutputStream(socket.getOutputStream());
                                 System.out.println("Sending request to Socket Server");
-                                User1.SetRecentCommand(Command);
+                                user.SetRecentCommand(Command);
 
                                 //Send Variables to the Server
                                 List<String> Info=new LinkedList<String>();
-                                Info.add(User1.GetUserName());
-                                Info.add(User1.GetRecentCommand());
+                                Info.add(user.GetUserName());
+                                Info.add(user.GetRecentCommand());
                                 oos.writeObject(Info);
 
                                 //Server Response
@@ -228,22 +224,11 @@ public class Main
                             }
                             finally
                             {
-                                if(WhichUser.contains("Root"))
-                                {
-                                    User1.SetRecentCommand(Command);
-                                    User1.SetRecentCommandDate(dtf.format(now));
-                                    User1.SetLog(User1.GetUserName()+" user wrote ("+User1.GetRecentCommand()+") command on "+User1.GetRecentCommandDate());
-                                    User1.SetListofCommandArgs(CommandArgs);
-                                    User1.SetOperatingSystem(OS);
-                                }
-                                else if(WhichUser.contains("Batuhan"))
-                                {
-                                    User2.SetRecentCommand(Command);
-                                    User2.SetRecentCommandDate(dtf.format(now));
-                                    User2.SetLog(User2.GetUserName()+" user wrote ("+User2.GetRecentCommand()+") command on "+User2.GetRecentCommandDate());
-                                    User2.SetListofCommandArgs(CommandArgs);
-                                    User2.SetOperatingSystem(OS);
-                                }
+                                user.SetRecentCommand(Command);
+                                user.SetRecentCommandDate(dtf.format(now));
+                                user.SetLog(user.GetUserName()+" user wrote ("+user.GetRecentCommand()+") command on "+user.GetRecentCommandDate());
+                                user.SetListofCommandArgs(CommandArgs);
+                                user.SetOperatingSystem(OS);
                             }
                         }
                     }
@@ -256,21 +241,26 @@ public class Main
                         System.out.println(ANSI_RED+"NULL");
                     }
                 }
-            }
 
-            //Get User Logs in the end of the program to see which command they used and when.
-            if(User1.GetLog() != null)
-            {
-                System.out.println(ANSI_CYAN+User1.GetLog());
-                User1.PrintUserCommands();
+                //Get User Logs in the end of the program to see which command they used and when.
+                if(user.GetLog() != null)
+                {
+                    System.out.println(ANSI_CYAN+user.GetLog());
+                    user.PrintUserCommands();
+                }
             }
-            if(User2.GetLog() != null)
+            else
             {
-                System.out.println(ANSI_CYAN+User2.GetLog());
-                User1.PrintUserCommands();
+                System.out.println(ANSI_RESET+ANSI_RED+"[Server]: "+ANSI_BLUE+"Wrong password."+ANSI_RESET);
             }
+        }
+        else
+        {
+            System.out.println(ANSI_RED+"You selected wrong username.");
+        }
 
             int Choose = 0;
+
             System.out.println(ANSI_RED+"Do you want to go back? Press 1 for reset program. Press anything except 1 for exit."+ANSI_RESET);
             try
             {
@@ -292,6 +282,5 @@ public class Main
                     break;
                 }
             }
-            //endregion
     }
 }
--- src/com/company/Miscellaneous.java
+++ src/com/company/Miscellaneous.java
@@ -3,9 +3,238 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import static com.company.Miscellaneous.*;
 
+//SQLite
+class SQLConnection
+{
+
+    /*
+    Note:
+    Do not forget to add sqlite-jbdc-3.34.0.jar to the libraries in any IDE.
+     */
+
+    /**
+     * Connect to a sample database
+     */
+    public static void connect(String DatabaseName)
+    {
+        Connection conn = null;
+        try
+        {
+            String url = "jdbc:sqlite:"+DatabaseName;
+            conn = DriverManager.getConnection(url);
+            System.out.println(ANSI_RESET+ANSI_RED+"[Server]: "+ANSI_RESET+ANSI_BLUE+"SQL connection is established.");
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
+            }
+            catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public static Connection ReturnConnect(String DatabaseName)
+    {
+        String url = "jdbc:sqlite:"+DatabaseName;
+        Connection conn = null;
+
+        try
+        {
+            conn = DriverManager.getConnection(url);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+
+    }
+
+    /**
+     * Connect to a sample database
+     *
+     * @param fileName the database file name
+     */
+    public static void createNewDatabase(String fileName)
+    {
+
+        String url = "jdbc:sqlite:" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url))
+        {
+            if (conn != null)
+            {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Create a new table in the test database (if not exist)
+     */
+    public static void createNewTable(String DatabaseName)
+    {
+        // SQLite connection string
+        String url = "jdbc:sqlite:"+DatabaseName;
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS Users (id integer PRIMARY KEY,UUID text NOT NULL,Username text NOT NULL,\tPassword TEXT NOT NULL, OperatingSystem TEXT NOT NULL)";
+
+        try (Connection conn = DriverManager.getConnection(url);Statement stmt = conn.createStatement())
+        {
+            stmt.execute(sql);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Insert User Info to the Users table in SQLite Database.
+     */
+    public static void InsertDatabase(String UUID,String Username, String Password, String OperatingSystem)
+    {
+
+        Boolean check = true;
+        String query = "SELECT * FROM Users where Username=? and Password=?";
+
+        try (Connection conn = SQLConnection.ReturnConnect("UsersDatabase.db");PreparedStatement pstmt = conn.prepareStatement(query))
+        {
+            pstmt.setString(1, Username);
+            pstmt.setString(2, Password);
+            ResultSet res = pstmt.executeQuery();
+
+            if(!res.next())
+            {
+                check = false;
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        if(!check)
+        {
+            String sql = "INSERT INTO Users(UUID,Username,Password,OperatingSystem) VALUES(?,?,?,?)";
+
+            try (Connection conn = SQLConnection.ReturnConnect("UsersDatabase.db");PreparedStatement pstmt = conn.prepareStatement(sql))
+            {
+                pstmt.setString(1, UUID);
+                pstmt.setString(2, Username);
+                pstmt.setString(3, Password);
+                pstmt.setString(4, OperatingSystem);
+                pstmt.executeUpdate();
+            }
+            catch (SQLException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+    /*
+    public static void Login(String Username, String Password)
+    {
+        String query = "SELECT * FROM Users where Username=? and Password=?";
+
+        try (Connection conn = SQLConnection.ReturnConnect("UsersDatabase.db");PreparedStatement pstmt = conn.prepareStatement(query))
+        {
+            PreparedStatement stmt = conn.prepareStatement(query);
+
+            stmt.setString(1, Username);
+            stmt.setString(2, Password);
+            stmt.executeQuery();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    } */
+
+    public static User LoginDatabase(String Username, String Password)
+    {
+        User user = null;
+
+        String query = "SELECT * FROM Users where Username=? and Password=?";
+
+        try (Connection conn = SQLConnection.ReturnConnect("UsersDatabase.db");PreparedStatement pstmt = conn.prepareStatement(query))
+        {
+            pstmt.setString(1, Username);
+            pstmt.setString(2, Password);
+            ResultSet res = pstmt.executeQuery();
+
+            if(res.next())
+            {
+                String UUID = res.getString("UUID");
+                String OperatingSystem = res.getString("OperatingSystem");
+
+                user = new User(UUID,Password, Username,OperatingSystem);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        return user;
+    }
+
+    public static List<String> UsernamesAll = new ArrayList<>();
+    public static void ListAllUsers()
+    {
+        String query = "SELECT DISTINCT Username FROM Users";
+
+        try (Connection conn = SQLConnection.ReturnConnect("UsersDatabase.db");PreparedStatement pstmt = conn.prepareStatement(query))
+        {
+            ResultSet res = pstmt.executeQuery();
+
+            String TempUsersList = "";
+
+            while (res.next())
+            {
+                TempUsersList = TempUsersList + res.getString("Username")+",";
+                UsernamesAll.add((res.getString("Username")));
+            }
+
+            TempUsersList = TempUsersList.substring(0, TempUsersList.length() - 1);
+            System.out.println(ANSI_RESET+ANSI_RED+"[Users]: "+ANSI_PURPLE+TempUsersList);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+//
 public class Miscellaneous
 {
     //region Variables
@@ -78,24 +307,22 @@ public class Miscellaneous
 class User
 {
     //Variables
-    private String UUID,RecentCommand,Log,Password,UserName,RecentCommandDate,UserSessionID,OperatingSystem;
+    private String UUID,RecentCommand,Log,Password,UserName,RecentCommandDate,OperatingSystem;
     private List<String> ListofCommandArgs;
 
     //Constructor
-    public User(String _UUID, String _Password, String _Username, String _UserSessionID)
+    public User(String _UUID, String _Password, String _Username)
     {
         UUID = _UUID;
         Password = _Password;
         UserName = _Username;
-        UserSessionID = _UserSessionID;
     }
 
-    public User(String _UUID, String _Password, String _Username, String _UserSessionID,String _RecentCommand)
+    public User(String _UUID, String _Password, String _Username,String _RecentCommand)
     {
         UUID = _UUID;
         Password = _Password;
         UserName = _Username;
-        UserSessionID = _UserSessionID;
     }
 
     //Getter Setters
@@ -129,8 +356,6 @@ class User
     {
         return UserName;
     }
-
-    public String GetUserSessionID() {return UserSessionID; }
 
     public List<String> GetListofCommandArgs()
     {
@@ -173,8 +398,6 @@ class User
     {
         this.UserName = _UserName;
     }
-
-    public void SetUserSessionID(String _SessionID) {this.UserSessionID = _SessionID; }
 
     //Print User Info Function
     public void PrintUserCommands()
